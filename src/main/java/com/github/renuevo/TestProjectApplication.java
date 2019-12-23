f5e1addbff81d55c0e3efa44ca2cdd0369c2e5cb
renuevo <renuevo0110@gmail.com>
2019-12-24 00:21:40 +0900

    Test Project Init

---
+++ src/main/java/com/github/renuevo/TestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.renuevo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProjectApplication.class, args);
+    }
+
+}
